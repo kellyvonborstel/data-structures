4459964adcf766caa68e9d192e293399ba4816fe
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-12 18:54:12 -0700

    complete stack.js prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -13,15 +13,11 @@ var stackMethods = {
   pop: function() {
     if (this.stackSize > 0) {
       this.stackSize--;
-      var value = this[this.stackSize];
-      return value;
+      return this[this.stackSize];
     }
-
   },
 
   size: function() {
     return this.stackSize;
   }
 };
-
-
