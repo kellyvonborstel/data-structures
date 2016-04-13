1b59499b9f0f3d0a3855f47b7d92b0d10e6632fa
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-12 22:03:52 -0700

    complete tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,42 @@
-var Tree = function(value){
+var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
-
+treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target) {
+  var found = false;
+  var checkNode = function(tree) {
+    if (tree.value === target) {
+      found = true;
+      return;
+    }
+    _.each(tree.children, function(child) {
+      checkNode(child);
+    });
+  };
+  checkNode(this);
+  return found;
+};
 
+treeMethods.traverse = function(callback) {
+  callback(this.value);
+  _.each(this.children, function(child) {
+    child.traverse(callback);
+  });
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// Time complexity for each function
+// addChild - constant - O(1)
+// contains - linear - O(n)
+// traverse - linear - O(n)
