a178b97de9fc87aa9a402f05a83f37a62f4e4cbe
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-12 20:30:15 -0700

    complete linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,29 +1,49 @@
-var LinkedList = function(){
+var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    var newTail = Node(value);
+    if (!list.head) {
+      list.head = newTail;
+    } else {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
   };
 
-  list.removeHead = function(){
+  list.removeHead = function() {
+    if (list.head) {
+      var value = list.head.value;
+      list.head = list.head.next;
+      return value;
+    }
   };
 
-  list.contains = function(target){
+  list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value){
+var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+// Time complexity for each function
+// addToTail - constant - O(1)
+// removeHead - constant - O(1)
+// contains - linear - O(n)
