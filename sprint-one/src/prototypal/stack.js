4099787dcb3a1fbb975439ff0f2a1c0f23ac0313
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-12 18:46:16 -0700

    refactor stack.js functional shared

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.stackSize = 0;
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function() {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      var value = this[this.stackSize];
+      return value;
+    }
+
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+};
 
 
